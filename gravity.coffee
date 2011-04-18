7d044877862b25d3e9b9d0aeafb08126314928bd
Sijmen Mulder <sjmulder@gmail.com>
2011-04-19 00:03:07 +0100

    Initial commit

---
+++ gravity.coffee
@@ -0,0 +1,99 @@
+settings = 
+	numBodies: 20
+	spawnArea: { x: 100, y: 100 }
+	worldSize: { x: 800, y: 500 }
+	gravity:   3
+	bodyMass:  3
+	step:      30
+	wrap:      false
+
+sim = null
+
+rand = (max) =>
+	Math.floor((Math.random() * max) + 1)
+
+seedBodies = =>
+	xmin = settings.worldSize.x / 2 - settings.spawnArea.x / 2
+	ymin = settings.worldSize.y / 2 - settings.spawnArea.y / 2
+	bodies = for i in [0...settings.numBodies]
+		body =
+			id: i
+			pos:
+				x: xmin + rand(settings.spawnArea.x)
+				y: ymin + rand(settings.spawnArea.y)
+			velocity:
+				x: 0
+				y: 0
+
+gravitationalAcceleration = (left, right) =>
+	m = settings.bodyMass
+	g = settings.gravity
+	dx = right.pos.x - left.pos.x
+	dy = right.pos.y - left.pos.y
+	r2 = (dx * dx) + (dy * dy)
+	acceleration = 
+ 		x: g * m / r2 * dx
+ 		y: g * m / r2 * dy
+
+wrapBody = (body, worldSize) =>
+	body.pos.x = (body.pos.x + worldSize.x) % worldSize.x
+	body.pos.y = (body.pos.y + worldSize.y) % worldSize.y	
+
+applyAcceleration = (body, acceleration, t) =>	
+	newVelocity =
+		x: body.velocity.x + acceleration.x * t
+		y: body.velocity.y + acceleration.y * t
+	displacement = 
+		x: (body.velocity.x + newVelocity.x) * t / 2
+		y: (body.velocity.y + newVelocity.y) * t / 2
+	body.velocity = newVelocity
+	body.pos.x += displacement.x
+	body.pos.y += displacement.y
+
+step = (sim) =>
+	t = 1 / settings.step
+	accelerations = {}
+	for i1 in [0...sim.bodies.length]
+		for i2 in [(i1 + 1)...sim.bodies.length]
+			left  = sim.bodies[i1]
+			right = sim.bodies[i2]
+			accelerations[left.id]  or= []
+			accelerations[right.id] or= []
+			leftAcceleration = gravitationalAcceleration(left, right)
+			rightAcceleration =
+				x: -leftAcceleration.x
+				y: -leftAcceleration.x
+			accelerations[left.id] .push(leftAcceleration)
+			accelerations[right.id].push(rightAcceleration)
+	for bodyId, bodyAccelerations of accelerations
+		body = sim.bodies[bodyId]
+		acceleration = { x: 0, y: 0 }
+		for bodyAcceleration in bodyAccelerations
+			acceleration.x += bodyAcceleration.x
+			acceleration.y += bodyAcceleration.y
+		applyAcceleration(body, acceleration, t)
+		wrapBody(body, settings.worldSize) if settings.wrap
+
+draw = (sim) =>
+	sim.context.clearRect(0, 0, settings.worldSize.x, settings.worldSize.y)
+	for body in sim.bodies
+		sim.context.fillRect(body.pos.x - 1, body.pos.y - 1, 3, 3)
+
+run = (sim) =>
+	draw(sim)
+	runfunc = =>
+		step(sim)
+		draw(sim)
+	setInterval(runfunc, 1 / settings.step)
+
+$ =>
+	sim = {}
+	sim.canvas  = $('.game').get(0) 
+	sim.context = sim.canvas.getContext('2d')
+	sim.bodies  = seedBodies()
+	run(sim)
+
+$('.game').click =>
+	if sim
+		step(sim)
+		draw(sim)
---
+++ gravity.js
@@ -0,0 +1,146 @@
+(function() {
+  var applyAcceleration, draw, gravitationalAcceleration, rand, run, seedBodies, settings, sim, step, wrapBody;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  settings = {
+    numBodies: 20,
+    spawnArea: {
+      x: 100,
+      y: 100
+    },
+    worldSize: {
+      x: 800,
+      y: 500
+    },
+    gravity: 3,
+    bodyMass: 3,
+    step: 30,
+    wrap: false
+  };
+  sim = null;
+  rand = __bind(function(max) {
+    return Math.floor((Math.random() * max) + 1);
+  }, this);
+  seedBodies = __bind(function() {
+    var bodies, body, i, xmin, ymin;
+    xmin = settings.worldSize.x / 2 - settings.spawnArea.x / 2;
+    ymin = settings.worldSize.y / 2 - settings.spawnArea.y / 2;
+    return bodies = (function() {
+      var _ref, _results;
+      _results = [];
+      for (i = 0, _ref = settings.numBodies; (0 <= _ref ? i < _ref : i > _ref); (0 <= _ref ? i += 1 : i -= 1)) {
+        _results.push(body = {
+          id: i,
+          pos: {
+            x: xmin + rand(settings.spawnArea.x),
+            y: ymin + rand(settings.spawnArea.y)
+          },
+          velocity: {
+            x: 0,
+            y: 0
+          }
+        });
+      }
+      return _results;
+    })();
+  }, this);
+  gravitationalAcceleration = __bind(function(left, right) {
+    var acceleration, dx, dy, g, m, r2;
+    m = settings.bodyMass;
+    g = settings.gravity;
+    dx = right.pos.x - left.pos.x;
+    dy = right.pos.y - left.pos.y;
+    r2 = (dx * dx) + (dy * dy);
+    return acceleration = {
+      x: g * m / r2 * dx,
+      y: g * m / r2 * dy
+    };
+  }, this);
+  wrapBody = __bind(function(body, worldSize) {
+    body.pos.x = (body.pos.x + worldSize.x) % worldSize.x;
+    return body.pos.y = (body.pos.y + worldSize.y) % worldSize.y;
+  }, this);
+  applyAcceleration = __bind(function(body, acceleration, t) {
+    var displacement, newVelocity;
+    newVelocity = {
+      x: body.velocity.x + acceleration.x * t,
+      y: body.velocity.y + acceleration.y * t
+    };
+    displacement = {
+      x: (body.velocity.x + newVelocity.x) * t / 2,
+      y: (body.velocity.y + newVelocity.y) * t / 2
+    };
+    body.velocity = newVelocity;
+    body.pos.x += displacement.x;
+    return body.pos.y += displacement.y;
+  }, this);
+  step = __bind(function(sim) {
+    var acceleration, accelerations, body, bodyAcceleration, bodyAccelerations, bodyId, i1, i2, left, leftAcceleration, right, rightAcceleration, t, _i, _len, _name, _name2, _ref, _ref2, _ref3, _results;
+    t = 1 / settings.step;
+    accelerations = {};
+    for (i1 = 0, _ref = sim.bodies.length; (0 <= _ref ? i1 < _ref : i1 > _ref); (0 <= _ref ? i1 += 1 : i1 -= 1)) {
+      for (i2 = _ref2 = i1 + 1, _ref3 = sim.bodies.length; (_ref2 <= _ref3 ? i2 < _ref3 : i2 > _ref3); (_ref2 <= _ref3 ? i2 += 1 : i2 -= 1)) {
+        left = sim.bodies[i1];
+        right = sim.bodies[i2];
+        accelerations[_name = left.id] || (accelerations[_name] = []);
+        accelerations[_name2 = right.id] || (accelerations[_name2] = []);
+        leftAcceleration = gravitationalAcceleration(left, right);
+        rightAcceleration = {
+          x: -leftAcceleration.x,
+          y: -leftAcceleration.x
+        };
+        accelerations[left.id].push(leftAcceleration);
+        accelerations[right.id].push(rightAcceleration);
+      }
+    }
+    _results = [];
+    for (bodyId in accelerations) {
+      bodyAccelerations = accelerations[bodyId];
+      body = sim.bodies[bodyId];
+      acceleration = {
+        x: 0,
+        y: 0
+      };
+      for (_i = 0, _len = bodyAccelerations.length; _i < _len; _i++) {
+        bodyAcceleration = bodyAccelerations[_i];
+        acceleration.x += bodyAcceleration.x;
+        acceleration.y += bodyAcceleration.y;
+      }
+      applyAcceleration(body, acceleration, t);
+      _results.push(settings.wrap ? wrapBody(body, settings.worldSize) : void 0);
+    }
+    return _results;
+  }, this);
+  draw = __bind(function(sim) {
+    var body, _i, _len, _ref, _results;
+    sim.context.clearRect(0, 0, settings.worldSize.x, settings.worldSize.y);
+    _ref = sim.bodies;
+    _results = [];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      body = _ref[_i];
+      _results.push(sim.context.fillRect(body.pos.x - 1, body.pos.y - 1, 3, 3));
+    }
+    return _results;
+  }, this);
+  run = __bind(function(sim) {
+    var runfunc;
+    draw(sim);
+    runfunc = __bind(function() {
+      step(sim);
+      return draw(sim);
+    }, this);
+    return setInterval(runfunc, 1 / settings.step);
+  }, this);
+  $(__bind(function() {
+    sim = {};
+    sim.canvas = $('.game').get(0);
+    sim.context = sim.canvas.getContext('2d');
+    sim.bodies = seedBodies();
+    return run(sim);
+  }, this));
+  $('.game').click(__bind(function() {
+    if (sim) {
+      step(sim);
+      return draw(sim);
+    }
+  }, this));
+}).call(this);
